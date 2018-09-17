17a88ad9224f124b9b57bb5ed9259e3a8c2eea8a
Wenjing <41601363+raowenjing@users.noreply.github.com>
2018-09-17 13:17:28 -0500

    modifications all over
    $s, mem 0x2000, finish loop, etc

--- example2.asm
+++ example2.asm
@@ -1,30 +1,32 @@
 #ECE 366 Sample Program 2
 # Author: Trung Le,  Wenjing Rao
 
-#	This program loads 2 numbers from data memory (say, X and Y ) and find the
-#	modulo of X%Y by substracting operation
+#	This program loads 2 numbers from data memory (say, X and Y ) 
+# 	finds the modulo of X%Y by substracting operation
+#	and store back the result into data memory (Z).
 
 .data 
-		array1: .word 2, 5		# X is first number,  Y is second number in the array
+		x: .word 240
+		y: .word 42	
+		z: .word -1	
 	
 .text
-		addi $t0, $t0, 0x10010000	# Base address of 'array1' stored in $t0
-		lw $t1, 0($t0)			# $t1 stores X
-		lw $t2, 4($t0)			# $t2 stores Y
+		addi $t0, $t0, 0x2000		# address of x = 0x2000 => $t0
+		lw $t1, 0($t0)			# now $t1 stores X (@ Mem[0x2000])
+		lw $t2, 4($t0)			# now $t2 stores Y (@ Mem[0x2004])
 loop:	
 		sub $t3,$t1,$t2			# t3 = t1 - t2 = X - Y
-		slt $a0, $t3,$0			# If X-Y < 0 , then we found the X%Y result
-		bne $a0,$0, finish
+		slt $s0, $t3,$0			# If X-Y < 0 , then we found the X%Y result
+		bne $s0,$0, done
 		sub $t1,$t1,$t2			# Else, we have not found the result yet, continue substracting
 						# until finished
 		j loop
 
 
-exit:		li $v0,10			# End program execution
-		syscall	
+exit:		j exit
 
 				
-finish:		
-		sw $t1,8($t0)
+done:		
+		sw $t1,8($t0)			# z is at Mem[0x2008]
 		j exit
 
