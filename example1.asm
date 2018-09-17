f01fdf027b265bcef8cc6575c6a7edd3fd9139de
raowenjing <41601363+raowenjing@users.noreply.github.com>
2018-09-17 14:24:01 -0500

    cosmetic changes for printing

--- example1.asm
+++ example1.asm
@@ -1,22 +1,22 @@
 #ECE 366 Sample Program 1
-# Author: Trung Le,  Wenjing Rao
+#Author: Trung Le,  Wenjing Rao
 #
 #This program checks the numbers in memory (array1 with 15 words) 
 #and stores back the absolute value of each number in array1.
 
+#Setting: memory configuration -> 
+#Compact (Text section begins at 0, Data begins at 0x2000)
 
-# setting: memory configuration -> 
-# Compact (Text section begins at 0, Data begins at 0x2000)
 .data
-	array1: .word -1, -2, 3, 5, -6, 0, 7, -2, 4, -7, -10, 1, -9, 11, 14
+	
+array1: .word -1, -2, 3, 5, -6, 0, 7, -2, 4, -7, -10, 1, -9, 11, 14
 	
 
 .text
 		addi $t0,$0,0x2000		# array1 starts @ 0x2000, $t0 (=$9) for base address
 		addi $t1,$0,15			# $t1 as counter: 15, 14, 13, ...1, 0
 		
-begin:
-		lw $s0,0($t0)			# Load each number in 'array1' to $s0
+begin:		lw $s0, 0($t0)			# Load each number in 'array1' to $s0
 		slt $s1, $s0, $0		# Compare if number < 0   ( NEGATIVE )				
 		bne $s1, $0, convert		# if so go to "convert" 
 
@@ -26,8 +26,7 @@ _convert:	sw $s0,0($t0)			# Store number back to array
 		beq $t1,$0,exit			# Are we finished?   If so (counter == 0) goto exit 
 		j begin				# else loop back
 
-convert: 
-		sub $s0,$0, $s0
+convert:	sub $s0,$0, $s0			
 		j _convert
 
 exit:		j exit				# program finished: dead loop 
